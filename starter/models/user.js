18e7388d241d7641435943f76fc5f3159e55bfc8
Aryawart-kathpal <aryawart.kathpal2909@gmail.com>
2023-12-24 21:25:01 +0530

    Setting up custom Errors

--- starter/models/user.js
+++ starter/models/user.js
@@ -12,6 +12,7 @@ const UserSchema = mongoose.Schema({
     },
     email:{
         type:String,
+        required:[true,"Please provide email"],
         // passing a regular Expression(regEx) which matches to email syntax
         match:[/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, "Please provide valid email"],
         unique:true,// generating a unique email everytime, avoiding duplicacy
